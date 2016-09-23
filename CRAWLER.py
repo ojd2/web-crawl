bf13ac5221fc4b2fb65749c123eb719396d895d0
Kino <kino@kino.lan>
2016-09-23 14:16:34 +0100

    Some final updates and added export methods

--- CRAWLER.py
+++ CRAWLER.py
@@ -9,35 +9,34 @@ import urllib2
 import urlparse
 from bs4 import BeautifulSoup
 
+# Begin Program:
 def crawl():
-	
 	# Set up root domain also known as the seed
     seed = "http://lipsum.com"
     # Confirmation before going any further into hell...
-    set_depth = str(raw_input("Would you like to crawl the following domain: " + seed + " (y/n): ")).lower().strip()
-    # GET header information...
-    print 'hello'
-
+    confirm_depth = str(raw_input("Would you like to crawl the following domain: " + seed + " (y/n): ")).lower().strip()
+    
+    
     # Crawl the entire website with no page depth limit
     # Will take a long time for large websites
-    if set_depth == "n":
+    if confirm_depth == "n":
         print "You shall not pass! You are going to be here a while dude?!\n"
         print "Exiting program..."
         exit()
 
     # Set a crawl web page depth limit
-    # Useful for large websites that take a long time to crawl
-    elif set_depth == 'y':
+    elif confirm_depth == 'y':
+
     	# For gocardless.com we will go 2, though 1 may be more appropriate and take less time
         depth_limit = 2
 
-        print "-" * 150
+        print "-" * 80
         print "Cool beans! Let's do this!\n\n"
-        print "Crawling in progress..."
+        print "Crawling in progress...\n\n"
 
         # Apply the same breadth-first search (BFS) algorithm except in this case
         # We need to to keep track of the depth level and stop the crawl when the
-        # Deepest level is reached
+        # Deepest level is reached:
         found_urls = []
         queue = Queue.Queue()
         queue.put(seed)
@@ -52,20 +51,17 @@ def crawl():
                     for link in getLinks(current_url, seed):
                         queue.put(link)
             time.sleep(1)
-        print "-" * 150
+        print "-" * 80
     else:
         print "Please select a valid option. Exiting..."
         sys.exit()
 
 
-# Method to extract all the urls embedded in a web page
-# given its url. the base url is also provided as a
-# parameter to determine whether the given url is internal
-# to the base url and not an external url to another website.
-# We are only interested in internal urls. if we follow external
-# urls our crawl will never end; however, if we discover external urls
-# with either of the words: 'test', 'dev', or 'stage' in them we will
-# print that information to the screen.
+# Here we fetch all hyperlinks embedded in the crawled base url. 
+# Function also double checks to see if url is base url or external
+# if it finds an external url then the url is passed as a parameter
+# to a function called exCheck() to print out the external url and
+# reasons for why it is external.
 def getLinks(parent_url, seed):
     child_urls = []
     try:
@@ -74,88 +70,102 @@ def getLinks(parent_url, seed):
         response = opener.open(parent_url)
         html = response.read()
         soup = BeautifulSoup(html, "lxml")
-        # href fields in anchor tags
+        # Find all <a> tags
         anchor_tag_links = soup("a")
         for link in anchor_tag_links:
             if link.has_attr("href"):
-                # convert url from a relative path to absolute
+                # Convert url from a relative path to absolute by joining 
+                # both parent and found link href address.
                 temp_url = urlparse.urljoin(parent_url, link["href"])
-                # we're only interested in crawling links in the same domain
-                # not external links like facebook, twitter, etc.
+                # If seed url is found in the temp_url, then great!
+                # If not, then send off the temp_url to the exCheck()
+                # function to then display why it was detected.
                 if seed in temp_url:
                     child_urls.append(temp_url)
                 else:
                     exCheck(temp_url)
-    # http request errors
+
+    # Some HTTP Request Errors may pop up! 
     except urllib2.HTTPError, e:
-        # ignore https errors like 404 - page not found and continue crawling
+        # If Error like 404 is requested, continue onwards...
         pass
-    # return a set version of the list to get rid of duplicate urls
+        
+
+    # Else, if not, continue on and return a set version of the list 
+    # to get rid of duplicate urls crawled already.
     return set(child_urls)
 
 
-# For urls that are external (not in the base domain) we will not
-# crawl them but we will print them to the screen if they have
-# one of these words in them: test, dev, or stage because they
-# may be of some interest...
+# Any urls found to be external, we shall list them and list why
+# they have been deemed as external.
 def exCheck(external_url):
-    if ("fr-fr" in external_url):
-        print "-----> Found External URL with the keyword: \'fr-fr\':", external_url
-    elif ("fr" in external_url):
-        print "-----> Found External URL with the keyword: \'fr-fr\':", external_url
-    elif ("se-se" in external_url):
-        print "-----> Found External URL with the keyword: \'se-se\':", external_url
-    elif ("se" in external_url):
-        print "-----> Found External URL with the keyword: \'se\':", external_url
-    elif ("support" in external_url):
-        print "-----> Found External URL with the keyword: \'support\':", external_url
+	# Set up an array for external keywords found in urls from website
+	ignore = ['support']
+	# For each item in the ignore list, if external_url matches
+	for item in ignore:
+		if item in external_url:
+			print "-----> Found External URL using keyword: " + item + ":", external_url 
+
 
 # A function to extract data from every url that comes up in our BFS crawl
-# algorithm. Using regular expressions we can extract several sigs of
-# data using certain patterns.
+# algorithm. Using regular expressions we can extract several types of
+# data using certain regular expression patterns.
 def exData(url):
-    #print "Found URL -----> " + url
-    # x = url 
-    # empty = []
-    # empty.append(x)
-    # print "Fetched links:" + str(empty)
-    try:
-        opener = urllib2.build_opener()
-        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-        response = opener.open(url)
-        html = response.read()
-        #print html
-        # get email addresses
-        findall(r'([\w\-\.]+@(\w[\w\-]+\.)+[\w\-]+)', url, html, 'Found Email Address:')
-        # get page titles
-        findall(r'<title[^>]*>([^<]+)</title>', url, html, 'Found Page Title:')
+ 	print "-----> Found URL: ", url
+    	try:
+        	opener = urllib2.build_opener()
+        	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        	response = opener.open(url)
+        	html = response.read()
+        	# Now parse any page title tags...
+        	soup = BeautifulSoup(html, "lxml")
+        	title = soup.find_all('title')
+        	# Send any page titles to the printMatch() with the parent url...
+        	printMatch(title, url, 'Found Page Title:')
 
-    except urllib2.HTTPError, e:
-        # ignore https errors like 404 - page not found and continue crawling
-        pass
+        	exp_title = str(title).strip("[]")
+    		exp_url = str(url)
+    		export = "\n" + "\n".join([exp_url, exp_title]) + "\n"
 
+    		final_export = list(export) # However, keep each item added, not overwrite?
+    		#print export
 
+        # If HTTP Request error here, then continue onwards again...
+    	except urllib2.HTTPError, e:
+        	pass
 
-# Prints method will print the data found to the screen if there is a match.
-def findall(regex, url, html, sig):
-    r = re.compile(regex, re.IGNORECASE)
-    link_matches = r.findall(html) # recursivley call
-    # On every page for each piece of information found we only want to display
-    # it once. To do that we keep track of links found using a list.
+# Print all matches found to GUI
+def printMatch(title, url, type):
+    # As we are passing an array of page titles, go through 
+    # each title and clean them up by removing <title> tags.
+    #for title_item in title:
+    title_matches = title
+    
+    # For every page visited, for matches found via regex above, print 
+    # page titles and the url it has found the match.
     matching_data = []
-    for link in link_matches:
-        if link not in matching_data:
-            matching_data.append(link)
-            if sig == 'Found Email Address:':
-                print 'Found URL -----> ' + url
-                print '----->', sig, link[0]
+    for title in title_matches:
+        if title not in matching_data:
+            matching_data.append(title)
+            if type == "Found Title Page:":
+                print " ---->", type, title[0]
             else:
-                print 'Found URL -----> ' + url
-                print '----->', sig, link
+            	print " ---->", type, title.get_text()
+    
+    
+    
+
+    # Here, I wish to take each item and push this into another data structure to 
+    # write the data and export to file...
+    
+
 
 		#exportData(sig, link)
 	
-# def exportData(sig,link):
+#def exportData(data):
+
+
+
 # 	rooturl = os.getcwd()
 # 	exporting = str(raw_input("Confirm the following $PATH:" + "\n" + rooturl + "\n" + "To export please select (y/n): ")).lower().strip()
 # 	if exporting[0] == "y":
@@ -173,5 +183,5 @@ def findall(regex, url, html, sig):
 # 	   print "Exiting Program..."
 # 	   exit()
 
-#callback
+# Callback
 crawl()

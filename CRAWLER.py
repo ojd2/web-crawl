5fcfc87320244ebb3a09da554bd8edcc1a83ed64
Kino <kino@kino.lan>
2016-09-23 17:02:51 +0100

    Tidy up and new comments. Final stages.

--- CRAWLER.py
+++ CRAWLER.py
@@ -1,46 +1,69 @@
-import re
-import requests
-import httplib
-import sys
-import os
-import time
+# ------------------------------------------------------------------------------
+# Task:
+# ------------------------------------------------------------------------------
+# Generate a Web Crawler to extract elements and build a simple Site Map.
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Solution:
+# ------------------------------------------------------------------------------
+# Following Program Crawls url(s) from the seed domain and traverses through.
+# It then presents a fetched url and its associated html page title and any
+# unwanted external url(s). This is then passed onto a print function to display 
+# the found and crawled url(s) and matches to the command GUI. The idea is to 
+# crawl using BFS and present any hierarchical site map distinctions. Such as, 
+# all the found url(s) from the seed, and then any external likes. The depth 
+# level can be adjusted to go deeper, however, this may present problems in 
+# execution and performance.
+
+# Besides this, the implementation of the BFS algorithm is also an educational 
+# experiment, to see how Breadth First crawling utilises the data structure of
+# 'queues', fetching and storing pages as it traverses from "lowest level in the 
+# in a graph" using comparative analysis of the lowest level by 
+# "then checking neighbour nodes step by step" (Patel & Jethva, 2015) until 
+# the whole graph has been traversed.
+# ------------------------------------------------------------------------------
+# List imports:
+# ------------------------------------------------------------------------------
 import Queue
+import sys
+import time
 import urllib2
 import urlparse
-from bs4 import BeautifulSoup
 
+from bs4 import BeautifulSoup
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 # Begin Program:
+# ------------------------------------------------------------------------------
 def crawl():
-	# Set up root domain also known as the seed
+    # Set up root domain also known as the seed.
     seed = "https://gocardless.com"
     # Confirmation before going any further into hell...
     confirm_depth = str(raw_input("Would you like to crawl the following domain: " + seed + " (y/n): ")).lower().strip()
-    
-    
-    # Crawl the entire website with no page depth limit
-    # Will take a long time for large websites
+
+    # If user selects 'n', then they can safely exit.
     if confirm_depth == "n":
         print "You shall not pass! You are going to be here a while dude?!\n"
         print "Exiting program..."
         exit()
 
-    # Set a crawl web page depth limit
+    # Set a crawl depth limit for our BSF algorithm:
     elif confirm_depth == 'y':
-
-    	# For gocardless.com we will go 2, though 1 may be more appropriate and take less time
-        depth_limit = 2
-
         print "-" * 80
         print "Cool beans! Let's do this!\n\n"
         print "Crawling in progress...\n\n"
 
         # Apply the same breadth-first search (BFS) algorithm except in this case
         # We need to to keep track of the depth level and stop the crawl when the
-        # Deepest level is reached:
+        # Deepest level is reached. For this particular url we will go with 2.
+        depth_limit = 2
+        # Set up some BFS variable requirements.
         found_urls = []
         queue = Queue.Queue()
         queue.put(seed)
         current_level = 1
+        # Begin the BFS Algorithm...
         while not queue.empty():
             current_url = queue.get()
             if current_url not in found_urls:
@@ -50,18 +73,20 @@ def crawl():
                     current_level = current_level + 1
                     for link in getLinks(current_url, seed):
                         queue.put(link)
+            # Suspend execution for a second.
             time.sleep(1)
         print "-" * 80
     else:
-        print "Please select a valid option. Exiting..."
-        sys.exit()
+        print "Please select a valid option. Either (y/n) please. \n Exiting Program..."
+        exit()
 
-
+# ------------------------------------------------------------------------------
 # Here we fetch all hyperlinks embedded in the crawled base url. 
 # Function also double checks to see if url is base url or external
 # if it finds an external url then the url is passed as a parameter
 # to a function called exCheck() to print out the external url and
 # reasons for why it is external.
+# ------------------------------------------------------------------------------
 def getLinks(parent_url, seed):
     child_urls = []
     try:
@@ -89,94 +114,76 @@ def getLinks(parent_url, seed):
     except urllib2.HTTPError, e:
         # If Error like 404 is requested, continue onwards...
         pass
-        
 
     # Else, if not, continue on and return a set version of the list 
-    # to get rid of duplicate urls crawled already.
+    # to get rid of duplicate url(s) crawled already.
     return set(child_urls)
 
-
-# Any urls found to be external, we shall list them and list why
-# they have been deemed as external.
+# ------------------------------------------------------------------------------
+# Any url(s) found to be external, we shall list them and list why
+# they have been deemed as external. So let's check them.
+# ------------------------------------------------------------------------------
 def exCheck(external_url):
-	# Set up an array for external keywords found in urls from website
-	ignore = ['support']
-	# For each item in the ignore list, if external_url matches
-	for item in ignore:
-		if item in external_url:
-			print "-----> Found External URL using keyword: " + item + ":", external_url 
+    # Set up an array for external keywords found in url(s) from website
+    ignore = ['support']
+    # For each item in the ignore list, if external_url matches
+    for item in ignore:
+        if item in external_url:
+            print "-----> Found External URL using keyword: " + item + ":", external_url
 
-
+# ------------------------------------------------------------------------------
 # A function to extract data from every url that comes up in our BFS crawl
 # algorithm. Using regular expressions we can extract several types of
-# data using certain regular expression patterns.
+# data using certain regular expression patterns. To generate a simple site 
+# map, we shall just extract the url(s) html <title> tags.
+# ------------------------------------------------------------------------------
 def exData(url):
- 	print "|-----> Found URL: ", url
-    	try:
-        	opener = urllib2.build_opener()
-        	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-        	response = opener.open(url)
-        	html = response.read()
-        	# Now parse any page title tags...
-        	soup = BeautifulSoup(html, "lxml")
-        	title = soup.find_all('title')
-        	# Send any page titles to the printMatch() with the parent url...
-        	printMatch(title, url, 'Found Page Title:')
+    print "|-----> Found URL: ", url
+    try:
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        response = opener.open(url)
+        html = response.read()
+        # Now parse any page title tags...
+        soup = BeautifulSoup(html, "lxml")
+        title = soup.find_all('title')
+        # Send any page titles to the printMatch() with the parent url...
+        printMatch(title, url, 'Found Page Title:')
 
-        	# Export both the parent url and the page title together into 
-        	# a list. Extend each time to keep hold of items every time the
-        	# function is called?
-        	exp_title = str(title).strip("[]")
-    		exp_url = str(url)
-    		export = "\n" + "\n".join([exp_url, exp_title]) + "\n"
+    # If HTTP Request error here, then continue onwards again...
+    except urllib2.HTTPError, e:
+        pass
 
-        # If HTTP Request error here, then continue onwards again...
-    	except urllib2.HTTPError, e:
-        	pass
-
-# Print all matches found to GUI
+# ------------------------------------------------------------------------------
+# Print all matches and associated url(s) found to GUI.
+# ------------------------------------------------------------------------------
 def printMatch(title, url, type):
     # As we are passing an array of page titles, go through 
     # each title and clean them up by removing <title> tags.
-    #for title_item in title:
+    # for title_item in title:
     title_matches = title
-    
+
     # For every page visited, for matches found via regex above, print 
     # page titles and the url it has found the match.
     matching_data = []
     for title in title_matches:
         if title not in matching_data:
-        	# Add both url and title to matching_data
+            # Add both url and title to matching_data
             matching_data.extend(title)
             if type == "Found Title Page:":
                 print " ->", type, title[0]
             else:
-            	print " ->", type, title.get_text()    
-
-    # Here, I wish to take each item and push this into another data structure to 
-    # write the data and export to file...
-	#exportData(sig, link)
-	
-#def exportData(data):
-
-
-
-# 	rooturl = os.getcwd()
-# 	exporting = str(raw_input("Confirm the following $PATH:" + "\n" + rooturl + "\n" + "To export please select (y/n): ")).lower().strip()
-# 	if exporting[0] == "y":
-# 		try:
-# 			fout = open('crawler-results.txt', 'w')
-# 		except:
-# 			print('File cannot be opened:', fname + '.txt')
-# 			exit()
-
-# 	crawled_data = link,sig # wrong - need to join these two into a list convert to string for exporting
-# 	fout.write(crawled_data)
-# 	print 'The following web crawler results have been exported to: ' + rooturl + '/crawler-results.txt'
-			    
-# 	if exporting[0] == "n":
-# 	   print "Exiting Program..."
-# 	   exit()
-
-# Callback
+                print " ->", type, title.get_text()
+# ------------------------------------------------------------------------------
+# Callback our Crawler
+# ------------------------------------------------------------------------------
 crawl()
+
+# ------------------------------------------------------------------------------
+# References:
+# ------------------------------------------------------------------------------
+
+# [1] Jitali Patel, Hardik Jethva, "Web Crawling". 2015. 
+# [2] IJIACS. ISSN 2347 8616. Volume 4, Special Issue.
+# [3] http://jakeaustwick.me/python-web-scraping-resource/#requests
+# [4] https://gist.github.com/duggalrahul/6548584
